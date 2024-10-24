31dfdc4d84fa078a896a5ee7fef051aec25df567
lewistong <66941436+lewistong@users.noreply.github.com>
2024-10-24 12:23:58 +0100

    added docs

--- src/components/MyButton.stories.js
+++ src/components/MyButton.stories.js
@@ -4,6 +4,14 @@ import MyButton from './MyButton';
 export default {
     title: 'Example/MyButton',
     component: MyButton,
+    parameters: {
+        docs: {
+            description: {
+                component: 'This is a button component that can be customized with props.',
+            },
+        },
+    },
+    tags: ['autodocs'],
 };
 
 const Template = (args) => <MyButton {...args} />;
@@ -12,4 +20,11 @@ export const Primary = Template.bind({});
 Primary.args = {
     label: 'Click Me',
     onClick: () => alert('Button clicked!'),
+};
+
+// Add a documentation section
+Primary.parameters = {
+    docs: {
+        storyDescription: 'This button is used to trigger actions.',
+    },
 };
